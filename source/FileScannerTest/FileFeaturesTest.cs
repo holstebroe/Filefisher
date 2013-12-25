fa16e5f70c1d4b9690a6888b2083c3dfd6103532
S�ren Holstebroe <holstebroe@gmail.com>
2013-12-25 22:24:47 +0100

    Added FileScanner project with FileFeatures stub.

---
+++ source/FileScannerTest/FileFeaturesTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace FileScannerTest
+{
+    [TestFixture]
+    public class FileFeaturesTest
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
