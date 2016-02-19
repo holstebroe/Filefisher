0b7a6d016c14a4bb0e5beee3df2afbbbd1223e88
Søren Holstebroe <holstebroe@gmail.com>
2016-02-19 15:43:57 +0100

    Added command line option parser

--- source/FilefisherConsole/Program.cs
+++ source/FilefisherConsole/Program.cs
@@ -2,6 +2,9 @@
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Text;
+using CommandLine;
+using CommandLine.Text;
 using FileScanner;
 
 namespace FilefisherConsole
@@ -10,29 +13,47 @@ namespace FilefisherConsole
     {
         static void Main(string[] args)
         {
-            if (args.Length == 0)
+            var options = new Options();
+            if (!Parser.Default.ParseArguments(args, options))
             {
-                ShowUsage();
+                // Display the default usage information
+//                Console.WriteLine(options.GetUsage());
+                Environment.Exit(1);
             }
-            var baseFolder = args[0];
+
+            var baseFolder = options.ScanFolder;
             if (!Directory.Exists(baseFolder))
             {
                 Console.WriteLine("No such directory: {0}", baseFolder);
                 ShowUsage();
             }
             var database = new MemoryFileDatabase();
+            if (!string.IsNullOrEmpty(options.ScanFolder))
+            {
+                var rootDescriptor = StatScanFolder(database, baseFolder);
+                if (options.SaveDatabase)
+                    database.SaveDefault();
+                if (options.Verbose)
+                {
+                    PrintDescriptorTree(rootDescriptor, descriptor => descriptor.StatHash);
+                    PrintDuplicates(database, descriptor => descriptor.StatHash);
+                }
+                if (options.ReadContent)
+                    UpdateContentSignatures(database, rootDescriptor);
+            }            
+        }
+
+        private static FileDescriptor StatScanFolder(MemoryFileDatabase database, string baseFolder)
+        {
             var signatureGenerator = new StatSignatureGenerator(new SHA1HashGenerator());
             var crawler = new FileCrawler(database, new SystemFileDescriptorProvider(), signatureGenerator);
             var scanTimer = Stopwatch.StartNew();
             var rootDescriptor = crawler.ScanDirectory(baseFolder);
             scanTimer.Stop();
-            database.SaveDefault();
-            PrintDescriptorTree(rootDescriptor, descriptor => descriptor.StatHash);
-            PrintDuplicates(database, descriptor => descriptor.StatHash);
             var descriptorCount = database.GetAllDescriptors().Count();
-            Console.WriteLine("Scanned {0} entries in {1}. {2} stat scans per second", descriptorCount, scanTimer.Elapsed, 1000 * descriptorCount / scanTimer.ElapsedMilliseconds);
-
-            UpdateContentSignatures(database, rootDescriptor);
+            Console.WriteLine("Scanned {0} entries in {1}. {2} stat scans per second", descriptorCount, scanTimer.Elapsed,
+                1000*descriptorCount/scanTimer.ElapsedMilliseconds);
+            return rootDescriptor;
         }
 
         private static void UpdateContentSignatures(MemoryFileDatabase database, FileDescriptor rootDescriptor)
@@ -86,6 +107,38 @@ namespace FilefisherConsole
         private static void ShowUsage()
         {
             Console.WriteLine("Filefisher <folder>");
+        }
+    }
+
+    class Options
+    {
+        [Option('f', "folder", Required = true, HelpText = "Folder to scan.")]
+        public string ScanFolder { get; set; }
+
+        [Option('c', "content", Required = false, HelpText = "Read content hash for each file")]
+        public bool ReadContent { get; set; }
+
+        [Option('s', "save", Required = false, HelpText = "Save scan to database file")]
+        public bool SaveDatabase { get; set; }
+
+        [Option('v', null, HelpText = "Print details during execution.")]
+        public bool Verbose { get; set; }
+
+        [HelpOption]
+        public string GetUsage()
+        {
+            return HelpText.AutoBuild(this, (HelpText current) => HelpText.DefaultParsingErrorsHandler(this, current));
+            //var help = new HelpText
+            //{
+            //    Heading = new HeadingInfo("<<app title>>", "<<app version>>"),
+            //    Copyright = new CopyrightInfo("<<app author>>", 2014),
+            //    AdditionalNewLineAfterOption = true,
+            //    AddDashesToOption = true
+            //};
+            //help.AddPreOptionsLine("<<license details here.>>");
+            //help.AddPreOptionsLine("Usage: app -p Someone");
+            //help.AddOptions(this);
+            //return help;
         }
     }
 }
